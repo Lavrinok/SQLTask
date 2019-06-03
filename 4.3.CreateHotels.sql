fdd6f137013fb49df7bf578576fae061ac71f5d0
Lavrinok <ilavrinok1999@gmail.com>
2019-06-03 21:27:32 +0300

    Second

--- 4.3.CreateHotels.sql
+++ 4.3.CreateHotels.sql
@@ -5,6 +5,7 @@ CREATE TABLE Hotels
 Id int NOT NULL PRIMARY KEY,
 Name varchar(50),
 FoundationYear int,
+Address varchar(30),
 IsActive bit
 );
 
--- 5.1.AddHotels.sql
+++ 5.1.AddHotels.sql
@@ -1,7 +1,7 @@
 USE sec;
 
-INSERT INTO Hotels(Id,Name,FoundationYear,IsActive)
+INSERT INTO Hotels(Id,Name,FoundationYear,Address,IsActive)
 VALUES
-		(1,'Willa',2012,1),
-        (2,'Edelweiss',2005,1),
-        (3,'Bukovyna',2050,1);
+		(1,'Willa',2012,'Popova',1),
+        (2,'Edelweiss',2005,'Lupova',1),
+        (3,'Bukovyna',2050,'Nagirna',1);
